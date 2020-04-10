096410aa3a9ce0de82972417547570bf77941ee7
naveen-verma-TTN <naveen.verma@tothenew.com>
2020-04-10 11:09:44 +0530

    Cloud Gallery (v1.0.0)

---
+++ app/src/main/java/com/minorproject/cloudgallery/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.minorproject.cloudgallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
