1d73eca388d14d836d7fb451442cd08f67a4cddd
naveen-verma-TTN <naveen.verma@tothenew.com>
2020-04-20 20:12:57 +0530

    User Profile upload (v1.0.0)

--- app/src/main/java/com/minorproject/cloudgallery/views/profile/UserProfile.kt
+++ app/src/main/java/com/minorproject/cloudgallery/views/profile/UserProfile.kt
@@ -95,7 +95,7 @@ class UserProfile : Fragment() {
     }
 
     companion object {
-        private val REQUEST_SELECT_IMAGE_IN_ALBUM = 101
+        private val REQUEST_SELECT_IMAGE_IN_ALBUM = 102
     }
 
     private fun selectImageInAlbum() {
